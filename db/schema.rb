786313b10746dc48c5e41fdf5a28db3bd04ac131
Rômulo Fortaleza <romulofortaleza_@hotmail.com>
2019-03-28 00:39:21 -0300

    cores azuis

--- db/schema.rb
+++ db/schema.rb
@@ -15,35 +15,6 @@ ActiveRecord::Schema.define(version: 2019_03_27_200539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "atendimentos", force: :cascade do |t|
-    t.bigint "paciente_id"
-    t.bigint "profissional_id"
-    t.bigint "unidade_id"
-    t.datetime "data"
-    t.text "anamnese"
-    t.integer "estado"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "procedimento_id"
-    t.index ["paciente_id"], name: "index_atendimentos_on_paciente_id"
-    t.index ["procedimento_id"], name: "index_atendimentos_on_procedimento_id"
-    t.index ["profissional_id"], name: "index_atendimentos_on_profissional_id"
-    t.index ["unidade_id"], name: "index_atendimentos_on_unidade_id"
-  end
-
-  create_table "clinicas", force: :cascade do |t|
-    t.string "descricao"
-    t.string "cnpj"
-    t.string "responsavel"
-    t.string "responsavel_cpf"
-    t.bigint "rede_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "endereco_id"
-    t.index ["endereco_id"], name: "index_clinicas_on_endereco_id"
-    t.index ["rede_id"], name: "index_clinicas_on_rede_id"
-  end
-
   create_table "customers", force: :cascade do |t|
     t.string "reason"
     t.string "cnpj"
@@ -51,33 +22,6 @@ ActiveRecord::Schema.define(version: 2019_03_27_200539) do
     t.string "mayor"
     t.string "cpf_mayor"
     t.string "city_hall"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "document_registers", force: :cascade do |t|
-    t.string "name"
-    t.bigint "instrument_registrations_id"
-    t.binary "document_file"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["instrument_registrations_id"], name: "index_document_registers_on_instrument_registrations_id"
-  end
-
-  create_table "enderecos", force: :cascade do |t|
-    t.string "logradouro"
-    t.integer "numero"
-    t.string "complemento"
-    t.string "bairro"
-    t.string "localidade"
-    t.string "uf"
-    t.string "cep"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "especialidades", force: :cascade do |t|
-    t.string "descricao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -108,87 +52,6 @@ ActiveRecord::Schema.define(version: 2019_03_27_200539) do
     t.string "instrument_type"
   end
 
-  create_table "pacientes", force: :cascade do |t|
-    t.string "nome"
-    t.string "rg"
-    t.string "cpf"
-    t.bigint "tipo_plano_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "endereco_id"
-    t.index ["endereco_id"], name: "index_pacientes_on_endereco_id"
-    t.index ["tipo_plano_id"], name: "index_pacientes_on_tipo_plano_id"
-  end
-
-  create_table "planos", force: :cascade do |t|
-    t.string "descricao"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "procedimentos", force: :cascade do |t|
-    t.integer "cid"
-    t.string "codigo"
-    t.string "descricao"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "especialidade_id"
-    t.index ["especialidade_id"], name: "index_procedimentos_on_especialidade_id"
-  end
-
-  create_table "profissionais", force: :cascade do |t|
-    t.string "nome"
-    t.string "cpf"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "especialidade_id"
-    t.bigint "endereco_id"
-    t.index ["endereco_id"], name: "index_profissionais_on_endereco_id"
-    t.index ["especialidade_id"], name: "index_profissionais_on_especialidade_id"
-  end
-
-  create_table "profissionais_unidades", id: false, force: :cascade do |t|
-    t.bigint "profissional_id", null: false
-    t.bigint "unidade_id", null: false
-  end
-
-  create_table "redes", force: :cascade do |t|
-    t.string "descricao"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
-  end
-
-  create_table "tipo_planos", force: :cascade do |t|
-    t.string "descricao"
-    t.bigint "plano_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["plano_id"], name: "index_tipo_planos_on_plano_id"
-  end
-
-  create_table "unidades", force: :cascade do |t|
-    t.string "descricao"
-    t.string "cnpj"
-    t.string "responsavel"
-    t.string "responsavel_cpf"
-    t.bigint "clinica_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "endereco_id"
-    t.index ["clinica_id"], name: "index_unidades_on_clinica_id"
-    t.index ["endereco_id"], name: "index_unidades_on_endereco_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -202,57 +65,4 @@ ActiveRecord::Schema.define(version: 2019_03_27_200539) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "usuarios", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "nome"
-    t.string "cpf", null: false
-    t.index ["cpf"], name: "index_usuarios_on_cpf", unique: true
-    t.index ["email"], name: "index_usuarios_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
-  end
-
-  create_table "usuarios_roles", id: false, force: :cascade do |t|
-    t.bigint "usuario_id"
-    t.bigint "role_id"
-    t.index ["role_id"], name: "index_usuarios_roles_on_role_id"
-    t.index ["usuario_id", "role_id"], name: "index_usuarios_roles_on_usuario_id_and_role_id"
-    t.index ["usuario_id"], name: "index_usuarios_roles_on_usuario_id"
-  end
-
-  create_table "valor_procedimentos", force: :cascade do |t|
-    t.bigint "clinica_id"
-    t.bigint "procedimento_id"
-    t.bigint "tipo_plano_id"
-    t.decimal "valor"
-    t.decimal "percentual_rateio_clinica"
-    t.decimal "percentual_rateio_profissional"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["clinica_id"], name: "index_valor_procedimentos_on_clinica_id"
-    t.index ["procedimento_id"], name: "index_valor_procedimentos_on_procedimento_id"
-    t.index ["tipo_plano_id"], name: "index_valor_procedimentos_on_tipo_plano_id"
-  end
-
-  add_foreign_key "atendimentos", "pacientes"
-  add_foreign_key "atendimentos", "procedimentos"
-  add_foreign_key "atendimentos", "profissionais", column: "profissional_id"
-  add_foreign_key "atendimentos", "unidades"
-  add_foreign_key "clinicas", "enderecos"
-  add_foreign_key "document_registers", "instrument_registrations", column: "instrument_registrations_id"
-  add_foreign_key "pacientes", "enderecos"
-  add_foreign_key "pacientes", "tipo_planos"
-  add_foreign_key "procedimentos", "especialidades"
-  add_foreign_key "profissionais", "enderecos"
-  add_foreign_key "profissionais", "especialidades"
-  add_foreign_key "tipo_planos", "planos"
-  add_foreign_key "unidades", "enderecos"
-  add_foreign_key "valor_procedimentos", "clinicas"
-  add_foreign_key "valor_procedimentos", "procedimentos"
-  add_foreign_key "valor_procedimentos", "tipo_planos"
 end
